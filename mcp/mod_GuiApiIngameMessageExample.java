4a93573f42ecd7488002d1204b8c35eac99ab84c
ShaRose <ShawnAndrewRose@Gmail.Com>
2012-04-05 16:46:52 -0230

    Added the Ingame Message example. Updated the ant script to accomadate it.

---
+++ mcp/mod_GuiApiIngameMessageExample.java
@@ -0,0 +1,33 @@
+package net.minecraft.src;
+
+/**
+ * This is a small example for how to make a user dialog. This part is just
+ * creating the item. Please view {@link ItemGuiApiExample} for the real code.
+ * 
+ * @author ShaRose
+ */
+public class mod_GuiApiIngameMessageExample extends BaseMod {
+
+	/**
+	 * A reference to the Example item.
+	 */
+	Item exampleItem;
+
+	@Override
+	public String getVersion() {
+		return "1.2.4";
+	}
+
+	@Override
+	public void load() {
+		exampleItem = new ItemGuiApiExample(30012).setItemName(
+				"GuiApiExampleItem").setIconCoord(10, 2);
+		// Create the example item, set the Name, and give it the sign's icon.
+		ModLoader.addName(exampleItem, "GuiAPI Example Item");
+		// Give it a 'real' name.
+		ModLoader.addShapelessRecipe(new ItemStack(exampleItem), new Object[] {
+				new ItemStack(Item.sign), new ItemStack(Item.paper) });
+		// Just a shapeless recipe. Normal sign and some paper.
+	}
+
+}
